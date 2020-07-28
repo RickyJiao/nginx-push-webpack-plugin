e3767b6637d5201e7bf9c3d761fa20c94c8fb6d8
Jiao <ricky.jiao@sap.com>
2020-07-28 11:18:47 +0800

    first commit

---
+++ typing.d.ts
@@ -0,0 +1,31 @@
+import { Compiler } from 'webpack';
+
+export = NginxPushWebpackPlugin;
+
+declare class NginxPushWebpackPlugin {
+  constructor(options?: NginxPushWebpackPlugin.Options);
+
+  apply(compiler: Compiler): void;
+}
+
+declare namespace NginxPushWebpackPlugin {
+  interface Options extends Partial<ProcessedOptions> { }
+
+  /**
+   * The plugin options after adding default values
+   */
+  interface ProcessedOptions {
+    /**
+     * The file to write the nginx conf to.
+     * Supports relative/absolute path eg: `conf/nginx.push.conf`, '/etc/conf/nginx/nginx.push.conf'
+     * @default 'nginx.push.conf'
+     */
+    filename: string;
+
+    /**
+     * Push static webpack bundle resource under what `location` in nginx
+     * @default 'index.html'
+     */
+    index: string;
+  }
+}
